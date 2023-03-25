8c70ad7a736955df448cd81d4383160804ffa1d5
ianklebold <ianstgo@gmail.com>
2023-03-25 14:47:58 -0300

    first commit

---
+++ src/main/java/com/chatgtp/api/ChatgtpApirestApplication.java
@@ -0,0 +1,13 @@
+package com.chatgtp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgtpApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgtpApirestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chatgtp/api/ChatgtpApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatgtp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgtpApirestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
